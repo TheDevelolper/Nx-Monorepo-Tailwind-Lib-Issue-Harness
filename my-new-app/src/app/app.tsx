8ae81bff55045741d94d6072006b69efbd09e202
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-08-05 16:30:36 +0100

    Minimal example of issue

---
+++ my-new-app/src/app/app.tsx
@@ -0,0 +1,13 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.css';
+import { TailwindMultiProjectExampleUiComponents } from '@tailwind-multi-project-example/ui-components';
+
+export function App() {
+  return (
+    <div>
+      <TailwindMultiProjectExampleUiComponents></TailwindMultiProjectExampleUiComponents>
+    </div>
+  );
+}
+
+export default App;
---
+++ ui-components/src/lib/ui-components.tsx
@@ -0,0 +1,9 @@
+export function TailwindMultiProjectExampleUiComponents() {
+  return (
+    <div className="bg-blue-500 text-white">
+      <h1>This should be blue with white text</h1>
+    </div>
+  );
+}
+
+export default TailwindMultiProjectExampleUiComponents;
